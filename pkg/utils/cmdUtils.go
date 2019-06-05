51a88cbb148dd9afb2395ac7d06821d6ad1c943a
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-06-05 11:47:39 +0200

    Use combined output for executing cmd

--- pkg/utils/cmdUtils.go
+++ pkg/utils/cmdUtils.go
@@ -9,11 +9,7 @@ import (
 // ExecuteCommand exectues the command using the args
 func ExecuteCommand(command string, args []string) (string, error) {
 	cmd := exec.Command(command, args...)
-	err := cmd.Run()
-	if err != nil {
-		return "", fmt.Errorf("Error executing command %s %s: %s", command, strings.Join(args, " "), err.Error())
-	}
-	out, err := cmd.Output()
+	out, err := cmd.CombinedOutput()
 	if err != nil {
 		return "", fmt.Errorf("Error executing command %s %s: %s", command, strings.Join(args, " "), err.Error())
 	}
