352ff59bbe0a469188b0f8d9e1c390d643df341f
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-06-05 11:45:54 +0200

    Wait until command completed

--- pkg/utils/cmdUtils.go
+++ pkg/utils/cmdUtils.go
@@ -8,7 +8,12 @@ import (
 
 // ExecuteCommand exectues the command using the args
 func ExecuteCommand(command string, args []string) (string, error) {
-	out, err := exec.Command(command, args...).Output()
+	cmd := exec.Command(command, args...)
+	err := cmd.Run()
+	if err != nil {
+		return "", fmt.Errorf("Error executing command %s %s: %s", command, strings.Join(args, " "), err.Error())
+	}
+	out, err := cmd.Output()
 	if err != nil {
 		return "", fmt.Errorf("Error executing command %s %s: %s", command, strings.Join(args, " "), err.Error())
 	}
