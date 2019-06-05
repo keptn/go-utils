22a45d95c43486caa9f1f4dec7a2dea3e0561f8b
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-06-05 15:32:04 +0200

    Export service name variable

--- pkg/utils/logger.go
+++ pkg/utils/logger.go
@@ -12,21 +12,22 @@ type keptnLogMessage struct {
 	LogLevel     string `json:"logLevel"`
 }
 
-const serviceName = "deploy-service"
+// ServiceName stores the name of the service
+var ServiceName = ""
 
 // Info logs an info message
 func Info(keptnContext string, message string) {
-	printLogMessage(keptnLogMessage{KeptnContext: keptnContext, Message: message, KeptnService: serviceName, LogLevel: "INFO"})
+	printLogMessage(keptnLogMessage{KeptnContext: keptnContext, Message: message, KeptnService: ServiceName, LogLevel: "INFO"})
 }
 
 // Error logs an error message
 func Error(keptnContext string, message string) {
-	printLogMessage(keptnLogMessage{KeptnContext: keptnContext, Message: message, KeptnService: serviceName, LogLevel: "ERROR"})
+	printLogMessage(keptnLogMessage{KeptnContext: keptnContext, Message: message, KeptnService: ServiceName, LogLevel: "ERROR"})
 }
 
 // Debug logs a debug message
 func Debug(keptnContext string, message string) {
-	printLogMessage(keptnLogMessage{KeptnContext: keptnContext, Message: message, KeptnService: serviceName, LogLevel: "DEBUG"})
+	printLogMessage(keptnLogMessage{KeptnContext: keptnContext, Message: message, KeptnService: ServiceName, LogLevel: "DEBUG"})
 }
 
 func printLogMessage(logMessage keptnLogMessage) {
