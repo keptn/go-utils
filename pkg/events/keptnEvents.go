1edf676aa54831e86c81758f5753363bac42ba8a
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-08-22 09:40:22 +0200

    #672 Collect keptn event data and types

---
+++ pkg/events/keptnEvents.go
@@ -0,0 +1,14 @@
+package events
+
+// ServiceCreateEventType is a CloudEvents type for creating new services
+const ServiceCreateEventType = "sh.keptn.event.service.create"
+
+// ServiceCreateEventData represents the
+type ServiceCreateEventData struct {
+	// Project is the name of the project
+	Project string `json:"project"`
+	// Service is the name of the new service
+	Service string `json:"service"`
+	// HelmChart is a base64 encoded Helm chart packed as tgz
+	HelmChart []byte `json:"helmChart"`
+}
