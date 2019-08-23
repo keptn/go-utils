7a8127de443f7ebd172efef8a9490c47a5c7fd6e
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-08-23 08:08:36 +0200

    #690 Configservice utils for service

---
+++ pkg/utils/serviceUtils.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+
+	"github.com/keptn/go-utils/pkg/models"
+)
+
+// ServiceHandler handles services
+type ServiceHandler struct {
+	BaseURL string
+}
+
+// NewServiceHandler returns a new ServiceHandler
+func NewServiceHandler(baseURL string) *ServiceHandler {
+	return &ServiceHandler{
+		BaseURL: baseURL,
+	}
+}
+
+// GetService returns a list of services.
+func (r *ResourceHandler) GetService(project string, stage string, pageSize int, nextPageKey string) (*models.Services, error) {
+	url, err := url.Parse("http://" + r.BaseURL + "/v1/project/" + project + "/stage/" + stage + "/service")
+	if err != nil {
+		return nil, err
+	}
+	q := url.Query()
+	q.Set("pageSize", strconv.Itoa(pageSize))
+	q.Set("nextPageKey", nextPageKey)
+
+	req, err := http.NewRequest("GET", url.String(), nil)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	var services models.Services
+	err = json.Unmarshal(body, &services)
+	return &services, err
+}
