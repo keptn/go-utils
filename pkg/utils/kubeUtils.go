ab949e428b65b0c0038bd11ab89a15367a262356
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-08-28 10:10:41 +0200

    #705 fix kube config

--- pkg/utils/kubeUtils.go
+++ pkg/utils/kubeUtils.go
@@ -129,7 +129,7 @@ func GetClientset(useInClusterConfig bool) (*kubernetes.Clientset, error) {
 // GetKeptnDomain reads the configmap keptn-domain in namespace keptn and returns
 // the contained app_domain
 func GetKeptnDomain(useInClusterConfig bool) (string, error) {
-	api, err := GetKubeAPI(true)
+	api, err := GetKubeAPI(useInClusterConfig)
 	if err != nil {
 		return "", err
 	}
