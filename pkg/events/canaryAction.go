713441a76d9e2528c9266ae59a1e6a33641215eb
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-08-29 12:49:50 +0200

    #727 New ConfigurationChangeEventData

---
+++ pkg/events/canaryAction.go
@@ -0,0 +1,54 @@
+package events
+
+import (
+	"bytes"
+	"encoding/json"
+)
+
+// CanaryAction describes the excution of a canary release
+type CanaryAction int
+
+const (
+	// Set is used for setting a new traffic weight for the canary
+	Set CanaryAction = iota
+	// Promote is used for promoting the canary
+	Promote
+	// Discard is used for discarding the canary
+	Discard
+)
+
+func (s CanaryAction) String() string {
+	return toString[s]
+}
+
+var toString = map[CanaryAction]string{
+	Set:     "Set",
+	Promote: "Promote",
+	Discard: "Discard",
+}
+
+var toID = map[string]CanaryAction{
+	"Set":     Set,
+	"Promote": Promote,
+	"Discard": Discard,
+}
+
+// MarshalJSON marshals the enum as a quoted json string
+func (s CanaryAction) MarshalJSON() ([]byte, error) {
+	buffer := bytes.NewBufferString(`"`)
+	buffer.WriteString(toString[s])
+	buffer.WriteString(`"`)
+	return buffer.Bytes(), nil
+}
+
+// UnmarshalJSON unmashals a quoted json string to the enum value
+func (s *CanaryAction) UnmarshalJSON(b []byte) error {
+	var j string
+	err := json.Unmarshal(b, &j)
+	if err != nil {
+		return err
+	}
+	// Note that if the string cannot be found then it will be set to the zero value, 'Created' in this case.
+	*s = toID[j]
+	return nil
+}
--- pkg/events/keptnEvents.go
+++ pkg/events/keptnEvents.go
@@ -1,5 +1,9 @@
 package events
 
+import (
+	"k8s.io/helm/pkg/proto/hapi/chart"
+)
+
 // ServiceCreateEventType is a CloudEvent type for creating new services
 const ServiceCreateEventType = "sh.keptn.event.service.create"
 
@@ -20,6 +24,30 @@ type ServiceCreateEventData struct {
 	Service string `json:"service"`
 	// HelmChart are the data of a Helm chart packed as tgz
 	HelmChart []byte `json:"helmChart"`
+}
+
+// ConfigurationChangeEventData represents the data for
+type ConfigurationChangeEventData struct {
+	// Project is the name of the project
+	Project string `json:"project"`
+	// Service is the name of the new service
+	Service string `json:"service"`
+	// Stage is the name of the stage
+	Stage string `json:"stage"`
+	// ValuesPrimary contains new Helm values for primary
+	ValuesPrimary map[string]*chart.Value `json:"valuesPrimary,omitempty"`
+	// ValuesCanary contains new Helm values for canary
+	ValuesCanary map[string]*chart.Value `json:"valuesCanary,omitempty"`
+	// Canary contains a new configuration for canary releases
+	Canary *Canary `json:"canary,omitempty"`
+}
+
+// Canary describes the new configuration in a canary release
+type Canary struct {
+	// Value represents the traffic percentage on the canary
+	Value int `json:"value,omitempty"`
+	// Action represents the action of the canary
+	Action CanaryAction `json:"action,omitempty"`
 }
 
 // ProblemEventData represents the data for describing a problem
