6e17331bc538b2018f045186fcd89fb66deb9419
agrimmer <andreas.grimmer@dynatrace.com>
2019-09-06 09:30:12 +0200

    #747 Combined logger

---
+++ pkg/utils/combined_logger.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+// CombinedLogger logs messages to the logger as well as to the websocket
+type CombinedLogger struct {
+	logger *Logger
+	ws     *websocket.Conn
+}
+
+// NewCombinedLogger creates a new CombinedLogger which writes log messages
+// to the logger as well as to the websocket
+func NewCombinedLogger(logger *Logger, ws *websocket.Conn) *CombinedLogger {
+	combinedLogger := CombinedLogger{
+		logger: logger,
+		ws:     ws,
+	}
+	return &combinedLogger
+}
+
+// Info logs an info message
+func (l *CombinedLogger) Info(message string, terminate bool) error {
+	l.logger.printLogMessage(keptnLogMessage{Timestamp: time.Now(), Message: message, LogLevel: "INFO"})
+	return WriteLog(l.ws, LogData{LogLevel: "INFO", Message: message, Terminate: terminate})
+}
+
+// Error logs an error message
+func (l *CombinedLogger) Error(message string, terminate bool) error {
+	l.logger.printLogMessage(keptnLogMessage{Timestamp: time.Now(), Message: message, LogLevel: "ERROR"})
+	return WriteLog(l.ws, LogData{LogLevel: "ERROR", Message: message, Terminate: terminate})
+}
+
+// Debug logs a debug message
+func (l *CombinedLogger) Debug(message string, terminate bool) error {
+	l.logger.printLogMessage(keptnLogMessage{Timestamp: time.Now(), Message: message, LogLevel: "DEBUG"})
+	return WriteLog(l.ws, LogData{LogLevel: "DEBUG", Message: message, Terminate: terminate})
+}
