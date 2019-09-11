0afa8fc8f089e3bfcc829737eb22be16c1a5003f
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2019-09-11 12:57:50 +0200

    add remediation strategy

--- pkg/models/shipyard.go
+++ pkg/models/shipyard.go
@@ -3,8 +3,9 @@ package models
 // Shipyard defines the name, deployment strategy and test strategy of each stage
 type Shipyard struct {
 	Stages []struct {
-		Name               string `json:"name" yaml:"name"`
-		DeploymentStrategy string `json:"deployment_strategy" yaml:"deployment_strategy"`
-		TestStrategy       string `json:"test_strategy,omitempty" yaml:"test_strategy"`
+		Name                string `json:"name" yaml:"name"`
+		DeploymentStrategy  string `json:"deployment_strategy" yaml:"deployment_strategy"`
+		TestStrategy        string `json:"test_strategy,omitempty" yaml:"test_strategy"`
+		RemediationStrategy string `json:"remediation_strategy,omitempty" yaml:"remediation_strategy"`
 	} `json:"stages" yaml:"stages"`
 }
