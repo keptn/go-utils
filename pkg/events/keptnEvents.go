329521914ca14df02d602724823aa6c5c4e5c9fb
Johannes <johannes.braeuer@dynatrace.com>
2019-09-18 11:07:15 +0200

    #671 Added omitempty and renamed json property name

--- pkg/events/keptnEvents.go
+++ pkg/events/keptnEvents.go
@@ -26,15 +26,15 @@ const ConfigureMonitoringEventType = "sh.keptn.event.monitoring.configure"
 // ProjectCreateEventData represents the data for creating a new project
 type ProjectCreateEventData struct {
 	// Project is the name of the project
-	Project string `json:"project"`
+	Project string `json:"project,"`
 	// Shipyard is a base64 encoded string of the shipyard file
 	Shipyard string `json:"shipyard"`
 	// GitUser is the name of a git user of an upstream repository
-	GitUser string `json:"gituser"`
+	GitUser string `json:"gitUser,omitempty"`
 	// GitToken is the authentication token for the git user
-	GitToken string `json:"gittoken"`
+	GitToken string `json:"gitToken,omitempty"`
 	// GitRemoteURL is the remote url of a repository
-	GitRemoteURL string `json:"gitremoteurl"`
+	GitRemoteURI string `json:"gitRemoteURI,omitempty"`
 }
 
 // ServiceCreateEventData represents the data for creating a new service
