e3bf2aa245e26e48acc51e3a55658dcbe89f7987
agrimmer <andreas.grimmer@dynatrace.com>
2020-05-10 15:50:39 +0200

    #1757 Introduce ApprovalStrategy struct

--- pkg/lib/shipyard.go
+++ pkg/lib/shipyard.go
@@ -3,13 +3,15 @@ package keptn
 // Shipyard defines the name, deployment strategy and test strategy of each stage
 type Shipyard struct {
 	Stages []struct {
-		Name                string `json:"name" yaml:"name"`
-		DeploymentStrategy  string `json:"deployment_strategy" yaml:"deployment_strategy"`
-		TestStrategy        string `json:"test_strategy,omitempty" yaml:"test_strategy"`
-		RemediationStrategy string `json:"remediation_strategy,omitempty" yaml:"remediation_strategy"`
-		ApprovalStrategy    *struct {
-			Pass    ApprovalStrategy `json:"pass,omitempty" yaml:"pass"`
-			Warning ApprovalStrategy `json:"warning,omitempty" yaml:"warning"`
-		} `json:"approval_strategy,omitempty" yaml:"approval_strategy"`
+		Name                string                  `json:"name" yaml:"name"`
+		DeploymentStrategy  string                  `json:"deployment_strategy" yaml:"deployment_strategy"`
+		TestStrategy        string                  `json:"test_strategy,omitempty" yaml:"test_strategy"`
+		RemediationStrategy string                  `json:"remediation_strategy,omitempty" yaml:"remediation_strategy"`
+		ApprovalStrategy    *ApprovalStrategyStruct `json:"approval_strategy,omitempty" yaml:"approval_strategy"`
 	} `json:"stages" yaml:"stages"`
+}
+
+type ApprovalStrategyStruct struct {
+	Pass    ApprovalStrategy `json:"pass,omitempty" yaml:"pass"`
+	Warning ApprovalStrategy `json:"warning,omitempty" yaml:"warning"`
 }
