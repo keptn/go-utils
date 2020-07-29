b2e964848b42f13c0a4ee5db9c8754f6ba3ecc85
Johannes <johannes.braeuer@dynatrace.com>
2020-07-29 07:14:39 +0200

    #2016 Struct for next generation of shipyard

---
+++ pkg/lib/v0_2_0/shipyard.go
@@ -0,0 +1,40 @@
+package v0_2_0
+
+///// v0.2.0 Shipyard Spec ///////
+
+// Shipyard describes a shipyard specification according to Keptn spec 0.2.0
+type Shipyard struct {
+	ApiVersion string           `json:"apiVersion" yaml:"apiVersion"`
+	Kind       string           `json:"kind" yaml:"kind"`
+	Metadata   ShipyardMetadata `json:"metadata" yaml:"metadata"`
+	Spec       ShipyardSpec     `json:"spec" yaml:"spec"`
+}
+
+// ShipyardMetadata describes Shipyayrd metadata
+type ShipyardMetadata struct {
+	Name string `json:"name" yaml:"name"`
+}
+
+// ShipyardSpec consists of any number of stages
+type ShipyardSpec struct {
+	Stages []Stage `json:"stages" yaml:"stages"`
+}
+
+// Stage defines a stage by its name and list of workflows
+type Stage struct {
+	Name      string     `json:"name" yaml:"name"`
+	Workflows []Workflow `json:"workflow" yaml:"workflow"`
+}
+
+// Workflow defines a workflow by its name and tasks. The listen property is optional
+type Workflow struct {
+	Name   string   `json:"name" yaml:"name"`
+	Listen []string `json:"listen" yaml:"listen"`
+	Tasks  []Task   `json:"tasks" yaml:"tasks"`
+}
+
+// Task defines a task by its name and optional properties
+type Task struct {
+	Name       string      `json:"name" yaml:"name"`
+	Properties interface{} `json:"properties" yaml:"properties"`
+}
