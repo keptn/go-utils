b4fabd54ef8868c63c47d40b42ebbed26e463c06
Florian Bacher <florian.bacher@dynatrace.com>
2020-11-04 15:53:24 +0100

    #2533 Adaptations for the delivery assistant workflow (#215)
    * #2533 added approval strategy approval.triggered event
    
    * #2533 added approval strategy approval.triggered event
    
    * #2533 added convenience method to retrieve open .triggered events
    
    * #2533 added convenience method to retrieve open .triggered events
    
    * #2533 fixed retrieval of events
    
    * #2533 added triggered id to keptn events

--- pkg/lib/v0_2_0/approval.go
+++ pkg/lib/v0_2_0/approval.go
@@ -2,8 +2,20 @@ package v0_2_0
 
 const ApprovalTaskName = "approval"
 
+// ApprovalAutomatic indicates an automatic approval strategy
+const ApprovalAutomatic = "automatic"
+
+// ApprovalManual indicates a manual approval strategy
+const ApprovalManual = "manual"
+
 type ApprovalTriggeredEventData struct {
 	EventData
+
+	// Approval contains information about the approval strategy
+	Approval struct {
+		Pass    string `json:"pass"`
+		Warning string `json:"warning"`
+	} `json:"approval"`
 }
 
 type ApprovalStartedEventData struct {
