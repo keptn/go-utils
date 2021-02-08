b3cb1d472e9cb29c6b5418e53087ba852c767985
agrimmer <andreas.grimmer@dynatrace.com>
2021-02-08 16:00:28 +0100

    Set accept header
    Signed-off-by: agrimmer <andreas.grimmer@dynatrace.com>

--- pkg/api/utils/resourceUtils.go
+++ pkg/api/utils/resourceUtils.go
@@ -278,7 +278,8 @@ func (r *ResourceHandler) getResource(uri string) (*models.Resource, error) {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	req, err := http.NewRequest("GET", uri, nil)
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Del("Accept-Encoding")
+	req.Header.Set("Accept", "application/json")
+
 	addAuthHeader(req, r)
 
 	resp, err := r.HTTPClient.Do(req)
