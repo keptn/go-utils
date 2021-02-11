5ed0bc0e8d5b0236c053d29220d939f7cdbbc0ae
Josh Soref <jsoref@users.noreply.github.com>
2021-02-10 20:04:19 -0500

    spelling: keptn
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- pkg/lib/v0_2_0/keptn.go
+++ pkg/lib/v0_2_0/keptn.go
@@ -167,7 +167,7 @@ func (k *Keptn) createCloudEventWithContextAndPayload(outEventType string, keptn
 	ce.SetID(uuid.New().String())
 	ce.SetType(outEventType)
 	ce.SetDataContentType(cloudevents.ApplicationJSON)
-	// use existing  kekptnContext for the new cloudevent
+	// use existing  keptnContext for the new cloudevent
 	ce.SetExtension(keptnContextCEExtension, keptnContext)
 	// the triggeredID links the sent event to the received .triggered event
 	ce.SetExtension(triggeredIDCEExtension, k.CloudEvent.ID())
