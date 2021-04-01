485323a967fc16b2b0a46ad27abd6a62b525a081
warber <bernd.warmuth@dynatrace.com>
2021-04-01 12:31:38 +0200

    introduced Downloader type
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- pkg/common/httputils/httputils.go
+++ pkg/common/httputils/httputils.go
@@ -1,17 +1,44 @@
 package httputils
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"time"
 )
 
-// DownloadFromURL downloads a file from the given url and returns
-// its content as a slice of bytes
-func DownloadFromURL(URL string) ([]byte, error) {
+type Downloader struct {
+	Timeout time.Duration
+}
+
+type DownloaderOption func(*Downloader)
+
+// WithTimeout sets a timeout on the underlying HTTP client
+func WithTimeout(timeout time.Duration) DownloaderOption {
+	return func(d *Downloader) {
+		d.Timeout = timeout
+	}
+}
+
+// NewDownloader creates a new Downloader to download files from an URL
+func NewDownloader(opts ...DownloaderOption) *Downloader {
+	d := &Downloader{}
+
+	for _, opt := range opts {
+		opt(d)
+	}
+	return d
+}
+
+// DownloadFromURL downloads a file from an URL and returns the content
+// as a slice of bytes
+func (d Downloader) DownloadFromURL(URL string) ([]byte, error) {
+	if !IsValidURL(URL) {
+		return nil, fmt.Errorf("%s is not a valid URL", URL)
+	}
 	c := http.Client{
-		Timeout: 5 * time.Second,
+		Timeout: d.Timeout,
 	}
 	resp, err := c.Get(URL)
 	if err != nil {
@@ -23,6 +50,12 @@ func DownloadFromURL(URL string) ([]byte, error) {
 		return nil, err
 	}
 	return body, nil
+}
+
+// DownloadFromURL downloads a file from an URL and returns the content
+// as a slice of bytes
+func DownloadFromURL(URL string) ([]byte, error) {
+	return NewDownloader().DownloadFromURL(URL)
 }
 
 // IsValidURL checks whether the given string is a valid URL or not
--- pkg/common/httputils/httputils_test.go
+++ pkg/common/httputils/httputils_test.go
@@ -2,8 +2,24 @@ package httputils
 
 import (
 	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
+
+func TestDownloadFromURL(t *testing.T) {
+
+	t.Run("File Download", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(200)
+			w.Write([]byte(":-)"))
+		}))
+		content, err := DownloadFromURL(server.URL)
+		assert.Nil(t, err)
+		assert.Equal(t, content, []byte(":-)"))
+	})
+
+}
 
 func TestIsValidURL(t *testing.T) {
 
