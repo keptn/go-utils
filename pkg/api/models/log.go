38f719c47463bec768277c8742df748b4d2a5d89
Florian Bacher <florian.bacher@dynatrace.com>
2021-06-01 12:09:42 +0200

    #4032 update data type
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- pkg/api/models/log.go
+++ pkg/api/models/log.go
@@ -1,7 +1,9 @@
 package models
 
+import "time"
+
 type LogEntry struct {
-	IntegrationID string `json:"integrationid" bson:"integrationid"`
-	Message       string `json:"message" bson:"message"`
-	Time          string `json:"time" bson:"time"`
+	IntegrationID string    `json:"integrationid" bson:"integrationid"`
+	Message       string    `json:"message" bson:"message"`
+	Time          time.Time `json:"time" bson:"time"`
 }
