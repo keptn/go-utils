93f439e861279d4a8f3d49bf768d526287df428a
Florian Bacher <florian.bacher@dynatrace.com>
2021-06-01 10:19:34 +0200

    #4032 rename property
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- pkg/api/models/log.go
+++ pkg/api/models/log.go
@@ -5,5 +5,5 @@ import "time"
 type LogEntry struct {
 	IntegrationID string    `json:"integrationid" bson:"integrationid"`
 	Message       string    `json:"message" bson:"message"`
-	Date          time.Time `json:"time" bson:"time"`
+	Time          time.Time `json:"time" bson:"time"`
 }
