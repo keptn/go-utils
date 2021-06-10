7fa5f65b5d90ab96d8a5e87c06ffc0ba8f199995
Florian Bacher <florian.bacher@dynatrace.com>
2021-06-10 13:05:30 +0200

    #4030 extended log data model
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- pkg/api/models/log.go
+++ pkg/api/models/log.go
@@ -6,6 +6,9 @@ type LogEntry struct {
 	IntegrationID string    `json:"integrationid" bson:"integrationid"`
 	Message       string    `json:"message" bson:"message"`
 	Time          time.Time `json:"time" bson:"time"`
+	KeptnContext  string    `json:"shkeptncontext" bson:"shkeptncontext"`
+	Task          string    `json:"task" bson:"task"`
+	TriggeredID   string    `json:"triggeredid" bson:"triggeredid"`
 }
 
 type GetLogsParams struct {
