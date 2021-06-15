ea559263a1b968bd1c7c5026482915c6ef2e25ae
Florian Bacher <florian.bacher@dynatrace.com>
2021-06-15 09:35:32 +0200

    #4306 rename uniqueIntegrationID to integrationID
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- pkg/lib/v0_2_0/log.go
+++ pkg/lib/v0_2_0/log.go
@@ -3,7 +3,7 @@ package v0_2_0
 const ErrorLogEventName = "sh.keptn.log.error"
 
 type ErrorLogEvent struct {
-	Message             string `json:"message"`
-	UniqueIntegrationID string `json:"uniqueIntegrationId"`
-	Task                string `json:"task"`
+	Message       string `json:"message"`
+	IntegrationID string `json:"IntegrationId"`
+	Task          string `json:"task"`
 }
