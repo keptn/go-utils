2a1ff066d2effd3cebf7e91157fbd7748dc51784
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2021-06-28 13:52:09 +0200

    keptn/keptn#4404 add documentation comments
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	// allow to load config from file
 	_ "embed"
 	"gopkg.in/yaml.v3"
 	"sync"
@@ -12,10 +13,12 @@ var cfg KeptnGoUtilsConfig
 
 var doOnce = sync.Once{}
 
+// KeptnGoUtilsConfig contains config for the keptn go-utils
 type KeptnGoUtilsConfig struct {
 	ShKeptnSpecVersion string `yaml:"shkeptnspecversion"`
 }
 
+// GetKeptnGoUtilsConfig take the config.yaml file and reads it into the KeptnGoUtilsConfig struct
 func GetKeptnGoUtilsConfig() KeptnGoUtilsConfig {
 	doOnce.Do(func() {
 		err := yaml.Unmarshal(yamlConfig, &cfg)
