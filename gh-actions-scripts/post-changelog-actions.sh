83918e7739342a83262e0e7d72936d50a8150d6e
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2021-07-22 15:31:43 +0200

    fix: use correct sed pattern to remove signoff messages from changelog
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- gh-actions-scripts/post-changelog-actions.sh
+++ gh-actions-scripts/post-changelog-actions.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 echo "Removing sign-off messages from changelog..."
-for file in ['CHANGELOG.md' 'RELEASE-BODY.md']; do
+for file in {CHANGELOG,RELEASE-BODY}.md; do
   if [ -f "$file" ]; then
-    sed ':a;N;$!ba;s/\nSigned-off-by: [A-Za-z0-9 ]*<.*@.*>\n//' "$file"
+    echo "Replacing content in $file"
+    # Reference: https://stackoverflow.com/a/1252191
+    sed -e ':a' -e 'N' -e '$!ba' -e 's/\nSigned-off-by: .* <.*@.*>\n/ /g' "$file" > tmp
+    mv tmp "$file"
+  else
+    echo "Not replacing anything since $file does not exist."
   fi
 done
