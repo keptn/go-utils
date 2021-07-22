f36121a7f9d03e528b609a3288334305f542c5c8
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2021-07-22 13:39:40 +0200

    feat: convert release config file to js for better readability
    BREAKING CHANGE: this is a test
    
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- .versionrc.js
+++ .versionrc.js
@@ -1,50 +1,53 @@
-{
-  "preMajor": true,
-  "issueUrlFormat": "https://github.com/keptn/keptn/issues/{{id}}",
-  "types": [
-    {
-      "type": "feat",
-      "section": "Features"
+module.exports = {
+    preMajor: true,
+    issueUrlFormat: "https://github.com/keptn/keptn/issues/{{id}}",
+    scripts: {
+        postchangelog: "./gh-actions-scripts/post-changelog-actions.sh"
     },
-    {
-      "type": "fix",
-      "section": "Bug Fixes"
-    },
-    {
-      "type": "chore",
-      "section": "Other"
-    },
-    {
-      "type": "docs",
-      "section": "Docs"
-    },
-    {
-      "type": "perf",
-      "section": "Performance"
-    },
-    {
-      "type": "build",
-      "hidden": true
-    },
-    {
-      "type": "ci",
-      "hidden": true
-    },
-    {
-      "type": "refactor",
-      "section": "Refactoring"
-    },
-    {
-      "type": "revert",
-      "hidden": true
-    },
-    {
-      "type": "style",
-      "hidden": true
-    },
-    {
-      "type": "test",
-      "hidden": true
-    }
-  ]
-}
+    types: [
+        {
+            type: "feat",
+            section: "Features"
+        },
+        {
+            type: "fix",
+            section: "Bug Fixes"
+        },
+        {
+            type: "chore",
+            section: "Other"
+        },
+        {
+            type: "docs",
+            section: "Docs"
+        },
+        {
+            type: "perf",
+            section: "Performance"
+        },
+        {
+            type: "build",
+            hidden: true
+        },
+        {
+            type: "ci",
+            hidden: true
+        },
+        {
+            type: "refactor",
+            section: "Refactoring"
+        },
+        {
+            type: "revert",
+            hidden: true
+        },
+        {
+            type: "style",
+            hidden: true
+        },
+        {
+            type: "test",
+            hidden: true
+        }
+    ]
+};
