47257796192b4745c881e500426781378e634b8e
Bernd Warmuth <72415058+warber@users.noreply.github.com>
2021-09-27 10:18:02 +0200

    feat: add temporary data to keptn event (#339)
    * implemented AddTemporaryData
    
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>
    
    * add additional unit test
    
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- pkg/api/models/keptn_context_extended_c_e.go
+++ pkg/api/models/keptn_context_extended_c_e.go
@@ -3,6 +3,7 @@ package models
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"time"
 )
 
@@ -72,4 +73,56 @@ func (ce *KeptnContextExtendedCE) Validate() error {
 		return errors.New("source must be specified")
 	}
 	return nil
+}
+
+// TemporaryData represents additional (temporary) data to be added
+// to the data section of a keptn event
+type TemporaryData interface{}
+
+// AddTemporaryDataOptions are used to modify the behavior of adding temporary
+// data to a keptn event
+type AddTemporaryDataOptions struct {
+	// OverwriteIfExisting indicates, that the data will be overwritten
+	// in case a key for that data already exists
+	OverwriteIfExisting bool
+}
+
+const temporaryDataRootKey = "temporaryData"
+
+// AddTemporaryData adds further (temporary) properties to the data section of the keptn event
+func (ce *KeptnContextExtendedCE) AddTemporaryData(key string, tmpData TemporaryData, opts AddTemporaryDataOptions) error {
+	eventData := map[string]interface{}{}
+	err := ce.DataAs(&eventData)
+	if err != nil {
+		return err
+	}
+	if temporaryData, found := eventData[temporaryDataRootKey]; found {
+		if _, kfound := temporaryData.(map[string]interface{})[key]; kfound {
+			if !opts.OverwriteIfExisting {
+				return fmt.Errorf("Key %s already exists", key)
+			}
+			temporaryData.(map[string]interface{})[key] = tmpData
+		}
+	} else {
+		eventData[temporaryDataRootKey] = map[string]interface{}{key: tmpData}
+	}
+	ce.Data = eventData
+	return nil
+}
+
+// GetTemporaryData returns the (temporary) data eventually stored in the event
+func (ce *KeptnContextExtendedCE) GetTemporaryData(key string, tmpdata interface{}) error {
+	eventData := map[string]interface{}{}
+	if err := ce.DataAs(&eventData); err != nil {
+		return err
+	}
+	if temporaryData, found := eventData[temporaryDataRootKey]; found {
+		if keyData, kfound := temporaryData.(map[string]interface{})[key]; kfound {
+			if marshalledKeyData, err := json.Marshal(keyData); err == nil {
+				return json.Unmarshal(marshalledKeyData, tmpdata)
+			}
+		}
+		return fmt.Errorf("temporary data with key %s not found", key)
+	}
+	return fmt.Errorf("temporary data with key %s not found", key)
 }
