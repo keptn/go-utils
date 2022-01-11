0d4f1cef56ac9af81788c66d93af9b4ce43e33a8
warber <bernd.warmuth@dynatrace.com>
2022-01-11 10:31:01 +0100

    godoc
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- pkg/api/utils/client.go
+++ pkg/api/utils/client.go
@@ -19,6 +19,7 @@ type KeptnInterface interface {
 	UniformV1() *UniformHandler
 }
 
+// ApiSet contains the API utils for all keptn APIs
 type ApiSet struct {
 	endpointURL            *url.URL
 	apiToken               string
@@ -36,77 +37,92 @@ type ApiSet struct {
 	shipyardControlHandler *ShipyardControllerHandler
 }
 
+// APIV1 retrieves the APIHandler
 func (c *ApiSet) APIV1() *APIHandler {
 	return c.apiHandler
 }
 
+// AuthV1 retrieves the AuthHandler
 func (c *ApiSet) AuthV1() *AuthHandler {
 	return c.authHandler
 }
 
+// EventsV1 retrieves the EventHandler
 func (c *ApiSet) EventsV1() *EventHandler {
 	return c.eventHandler
 }
 
+// LogsV1 retrieves the LogHandler
 func (c *ApiSet) LogsV1() *LogHandler {
 	return c.logHandler
 }
 
+// ProjectsV1 retrieves the ProjectHandler
 func (c *ApiSet) ProjectsV1() *ProjectHandler {
 	return c.projectHandler
 }
 
+// ResourcesV1 retrieves the ResourceHandler
 func (c *ApiSet) ResourcesV1() *ResourceHandler {
 	return c.resourceHandler
 }
 
+// SecretsV1 retrieves the SecretHandler
 func (c *ApiSet) SecretsV1() *SecretHandler {
 	return c.secretHandler
 }
 
+// SequencesV1 retrieves the SequenceControlHandler
 func (c *ApiSet) SequencesV1() *SequenceControlHandler {
 	return c.sequenceControlHandler
 }
 
+// ServicesV1 retrieves the ServiceHandler
 func (c *ApiSet) ServicesV1() *ServiceHandler {
 	return c.serviceHandler
 }
 
+// StagesV1 retrieves the StageHandler
 func (c *ApiSet) StagesV1() *StageHandler {
 	return c.stageHandler
 }
 
+// UniformV1 retrieves the UniformHandler
 func (c *ApiSet) UniformV1() *UniformHandler {
 	return c.uniformHandler
 }
 
+// ShipyardControlHandlerV1 retrieves the ShipyardControllerHandler
 func (c *ApiSet) ShipyardControlHandlerV1() *ShipyardControllerHandler {
 	return c.shipyardControlHandler
 }
 
+// Token retrieves the API token
 func (c *ApiSet) Token() string {
 	return c.apiToken
 }
 
+// Endpoint retrieves the base API endpoint URL
 func (c *ApiSet) Endpoint() *url.URL {
 	return c.endpointURL
 }
 
+// NewApiSet creates a new ApiSet
 func NewApiSet(baseURL string, authToken string, authHeader string, httpClient *http.Client, scheme string) (*ApiSet, error) {
 	var as ApiSet
-	as.apiHandler = NewAuthenticatedAPIHandler(baseURL, authToken, "x-token", httpClient, scheme)
-	as.authHandler = NewAuthenticatedAuthHandler(baseURL, authToken, "x-token", httpClient, scheme)
-	as.logHandler = NewAuthenticatedLogHandler(baseURL, authToken, "x-token", httpClient, scheme)
-	as.authHandler = NewAuthenticatedAuthHandler(baseURL, authToken, "x-token", httpClient, scheme)
-	as.eventHandler = NewAuthenticatedEventHandler(baseURL, authToken, "x-token", httpClient, scheme)
-	as.projectHandler = NewAuthenticatedProjectHandler(baseURL, authToken, "x-token", httpClient, scheme)
-	as.resourceHandler = NewAuthenticatedResourceHandler(baseURL, authToken, "x-token", httpClient, scheme)
-	as.secretHandler = NewAuthenticatedSecretHandler(baseURL, authToken, "x-token", httpClient, scheme)
-	as.sequenceControlHandler = NewAuthenticatedSequenceControlHandler(baseURL, authToken, "x-token", httpClient, scheme)
-	as.serviceHandler = NewAuthenticatedServiceHandler(baseURL, authToken, "x-token", httpClient, scheme)
-	as.shipyardControlHandler = NewAuthenticatedShipyardControllerHandler(baseURL, authToken, "x-token", httpClient, scheme)
-	as.stageHandler = NewAuthenticatedStageHandler(baseURL, authToken, "x-token", httpClient, scheme)
-	as.uniformHandler = NewAuthenticatedUniformHandler(baseURL, authToken, "x-token", httpClient, scheme)
+	as.apiHandler = NewAuthenticatedAPIHandler(baseURL, authToken, authHeader, httpClient, scheme)
+	as.authHandler = NewAuthenticatedAuthHandler(baseURL, authToken, authHeader, httpClient, scheme)
+	as.logHandler = NewAuthenticatedLogHandler(baseURL, authToken, authHeader, httpClient, scheme)
+	as.authHandler = NewAuthenticatedAuthHandler(baseURL, authToken, authHeader, httpClient, scheme)
+	as.eventHandler = NewAuthenticatedEventHandler(baseURL, authToken, authHeader, httpClient, scheme)
+	as.projectHandler = NewAuthenticatedProjectHandler(baseURL, authToken, authHeader, httpClient, scheme)
+	as.resourceHandler = NewAuthenticatedResourceHandler(baseURL, authToken, authHeader, httpClient, scheme)
+	as.secretHandler = NewAuthenticatedSecretHandler(baseURL, authToken, authHeader, httpClient, scheme)
+	as.sequenceControlHandler = NewAuthenticatedSequenceControlHandler(baseURL, authToken, authHeader, httpClient, scheme)
+	as.serviceHandler = NewAuthenticatedServiceHandler(baseURL, authToken, authHeader, httpClient, scheme)
+	as.shipyardControlHandler = NewAuthenticatedShipyardControllerHandler(baseURL, authToken, authHeader, httpClient, scheme)
+	as.stageHandler = NewAuthenticatedStageHandler(baseURL, authToken, authHeader, httpClient, scheme)
+	as.uniformHandler = NewAuthenticatedUniformHandler(baseURL, authToken, authHeader, httpClient, scheme)
 	as.apiToken = authToken
 	url, _ := url.Parse(baseURL)
 	as.endpointURL = url
