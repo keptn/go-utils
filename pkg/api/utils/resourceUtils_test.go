4dd9fbc862e78a743e333c9399087c8f8ca7f4d3
RealAnna <anna.reale@dynatrace.com>
2022-02-02 17:42:53 +0100

    fix: added test
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

---
+++ pkg/api/utils/resourceUtils_test.go
@@ -0,0 +1,79 @@
+package api
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestResourceHandler_buildResourceURI(t *testing.T) {
+	scheme := "https"
+	tests := []struct {
+		name     string
+		project  string
+		stage    string
+		service  string
+		resource string
+		want     string
+	}{
+		{
+			name:     "project resources",
+			project:  "myproject",
+			stage:    "",
+			service:  "",
+			resource: "",
+			want:     scheme + "://" + configurationServiceBaseUrl + v1ProjectPath + "/myproject/resource",
+		},
+		{
+			name:     "project resource",
+			project:  "myproject",
+			stage:    "",
+			service:  "",
+			resource: "metadata.yaml",
+			want:     scheme + "://" + configurationServiceBaseUrl + v1ProjectPath + "/myproject/resource/metadata.yaml",
+		},
+		{
+			name:     "stage resource",
+			project:  "sockshop",
+			stage:    "dev",
+			service:  "",
+			resource: "metadata.yaml",
+			want:     scheme + "://" + configurationServiceBaseUrl + v1ProjectPath + "/sockshop" + pathToStage + "/dev/resource/metadata.yaml",
+		},
+		{
+			name:     "service resource",
+			project:  "sockshop",
+			stage:    "dev",
+			service:  "helloservice",
+			resource: "hello.go",
+			want:     scheme + "://" + configurationServiceBaseUrl + v1ProjectPath + "/sockshop" + pathToStage + "/dev/service/helloservice/resource/hello.go",
+		},
+		{
+			name:     "service resources",
+			project:  "sockshop",
+			stage:    "dev",
+			service:  "helloservice",
+			resource: "",
+			want:     scheme + "://" + configurationServiceBaseUrl + v1ProjectPath + "/sockshop" + pathToStage + "/dev/service/helloservice/resource",
+		},
+		{
+			name:     "service resource escape / ",
+			project:  "sockshop",
+			stage:    "dev",
+			service:  "helloservice",
+			resource: "helm%2Fhelloservice.tgz",
+			want:     scheme + "://" + configurationServiceBaseUrl + v1ProjectPath + "/sockshop" + pathToStage + "/dev/service/helloservice/resource/helm%252Fhelloservice.tgz",
+		},
+	}
+
+	r := &ResourceHandler{
+		BaseURL: configurationServiceBaseUrl,
+		Scheme:  scheme,
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			scope := *NewResourceScope(tt.project, tt.stage, tt.service, tt.resource)
+			assert.Equalf(t, tt.want, r.buildResourceURI(scope), "buildResourceURI(%v)", scope)
+		})
+	}
+}
