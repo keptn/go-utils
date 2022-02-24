b472bbaf5dd2f090e022f0bac04b2788c7d39ffa
odubajDT <ondrej.dubaj@dynatrace.com>
2022-02-24 15:06:19 +0100

    feat: Support auth via proxy
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- pkg/api/models/create_project.go
+++ pkg/api/models/create_project.go
@@ -17,6 +17,21 @@ type CreateProject struct {
 	// git private key passphrase
 	GitPrivateKeyPass string `json:"gitPrivateKeyPass,omitempty"`
 
+	// git proxy URL
+	GitProxyURL string `json:"gitProxyUrl,omitempty"`
+
+	// git proxy scheme
+	GitProxyScheme string `json:"gitProxyScheme,omitempty"`
+
+	// git proxy user
+	GitProxyUser string `json:"gitProxyUser,omitempty"`
+
+	// git proxy secure
+	GitProxySecure bool `json:"gitProxySecure,omitempty"`
+
+	// git proxy password
+	GitProxyPassword string `json:"gitProxyPassword,omitempty"`
+
 	// git user
 	GitUser string `json:"gitUser,omitempty"`
 
