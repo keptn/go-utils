4e3979eb2ed4fa09dd9d8ba3f6db00eec3459ada
Arthur Pitman <arthur.pitman@dynatrace.com>
2022-04-19 10:52:27 +0200

    Add context to `SecretsV1Interface`
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- pkg/api/utils/secretUtils.go
+++ pkg/api/utils/secretUtils.go
@@ -14,13 +14,32 @@ const v1SecretPath = "/v1/secret"
 
 type SecretsV1Interface interface {
 	SecretHandlerInterface
+
+	// CreateSecretWithContext creates a new secret.
+	CreateSecretWithContext(ctx context.Context, secret models.Secret) error
+
+	// UpdateSecretWithContext creates a new secret.
+	UpdateSecretWithContext(ctx context.Context, secret models.Secret) error
+
+	// DeleteSecretWithContext deletes a secret.
+	DeleteSecretWithContext(ctx context.Context, secretName, secretScope string) error
+
+	// GetSecretsWithContext returns a list of created secrets.
+	GetSecretsWithContext(ctx context.Context) (*models.GetSecretsResponse, error)
 }
 
 //go:generate moq -pkg utils_mock -skip-ensure -out ./fake/secret_handler_mock.go . SecretHandlerInterface
 type SecretHandlerInterface interface {
+	// CreateSecret creates a new secret.
 	CreateSecret(secret models.Secret) error
+
+	// UpdateSecret creates a new secret.
 	UpdateSecret(secret models.Secret) error
+
+	// DeleteSecret deletes a secret.
 	DeleteSecret(secretName, secretScope string) error
+
+	// GetSecrets returns a list of created secrets.
 	GetSecrets() (*models.GetSecretsResponse, error)
 }
 
@@ -95,44 +114,64 @@ func (s *SecretHandler) getHTTPClient() *http.Client {
 	return s.HTTPClient
 }
 
-// CreateSecret creates a new secret
+// CreateSecret creates a new secret.
 func (s *SecretHandler) CreateSecret(secret models.Secret) error {
+	return s.CreateSecretWithContext(context.TODO(), secret)
+}
+
+// CreateSecretWithContext creates a new secret.
+func (s *SecretHandler) CreateSecretWithContext(ctx context.Context, secret models.Secret) error {
 	body, err := secret.ToJSON()
 	if err != nil {
 		return err
 	}
-	_, errObj := post(context.TODO(), s.Scheme+"://"+s.BaseURL+v1SecretPath, body, s)
+	_, errObj := post(ctx, s.Scheme+"://"+s.BaseURL+v1SecretPath, body, s)
 	if errObj != nil {
 		return errors.New(errObj.GetMessage())
 	}
 	return nil
 }
 
-// UpdateSecret creates a new secret
+// UpdateSecret creates a new secret.
 func (s *SecretHandler) UpdateSecret(secret models.Secret) error {
+	return s.UpdateSecretWithContext(context.TODO(), secret)
+}
+
+// UpdateSecretWithContext creates a new secret.
+func (s *SecretHandler) UpdateSecretWithContext(ctx context.Context, secret models.Secret) error {
 	body, err := secret.ToJSON()
 	if err != nil {
 		return err
 	}
-	_, errObj := put(context.TODO(), s.Scheme+"://"+s.BaseURL+v1SecretPath, body, s)
+	_, errObj := put(ctx, s.Scheme+"://"+s.BaseURL+v1SecretPath, body, s)
 	if errObj != nil {
 		return errors.New(errObj.GetMessage())
 	}
 	return nil
 }
 
-// DeleteSecret deletes a secret
+// DeleteSecret deletes a secret.
 func (s *SecretHandler) DeleteSecret(secretName, secretScope string) error {
-	_, err := delete(context.TODO(), s.Scheme+"://"+s.BaseURL+v1SecretPath+"?name="+secretName+"&scope="+secretScope, s)
+	return s.DeleteSecretWithContext(context.TODO(), secretName, secretScope)
+}
+
+// DeleteSecretWithContext deletes a secret.
+func (s *SecretHandler) DeleteSecretWithContext(ctx context.Context, secretName, secretScope string) error {
+	_, err := delete(ctx, s.Scheme+"://"+s.BaseURL+v1SecretPath+"?name="+secretName+"&scope="+secretScope, s)
 	if err != nil {
 		return errors.New(err.GetMessage())
 	}
 	return nil
 }
 
-// GetSecrets returns a list of created secrets
+// GetSecrets returns a list of created secrets.
 func (s *SecretHandler) GetSecrets() (*models.GetSecretsResponse, error) {
-	body, mErr := getAndExpectOK(context.TODO(), s.Scheme+"://"+s.BaseURL+v1SecretPath, s)
+	return s.GetSecretsWithContext(context.TODO())
+}
+
+// GetSecretsWithContext returns a list of created secrets.
+func (s *SecretHandler) GetSecretsWithContext(ctx context.Context) (*models.GetSecretsResponse, error) {
+	body, mErr := getAndExpectOK(ctx, s.Scheme+"://"+s.BaseURL+v1SecretPath, s)
 	if mErr != nil {
 		return nil, mErr.ToError()
 	}
