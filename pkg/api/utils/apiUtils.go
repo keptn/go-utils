62fa95dd31f1ab29aa51ab366ace3a3ee4f629ed
Arthur Pitman <arthur.pitman@dynatrace.com>
2022-05-06 15:38:30 +0200

    Introduce APIV2Interface
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- pkg/api/utils/apiUtils.go
+++ pkg/api/utils/apiUtils.go
@@ -15,54 +15,85 @@ type APIV1Interface interface {
 	// SendEvent sends an event to Keptn.
 	SendEvent(event models.KeptnContextExtendedCE) (*models.EventContext, *models.Error)
 
-	// SendEventWithContext sends an event to Keptn.
-	SendEventWithContext(ctx context.Context, event models.KeptnContextExtendedCE) (*models.EventContext, *models.Error)
-
 	// TriggerEvaluation triggers a new evaluation.
 	TriggerEvaluation(project string, stage string, service string, evaluation models.Evaluation) (*models.EventContext, *models.Error)
-
-	// TriggerEvaluationWithContext triggers a new evaluation.
-	TriggerEvaluationWithContext(ctx context.Context, project string, stage string, service string, evaluation models.Evaluation) (*models.EventContext, *models.Error)
 
 	// CreateProject creates a new project.
 	CreateProject(project models.CreateProject) (string, *models.Error)
 
-	// CreateProjectWithContext creates a new project.
-	CreateProjectWithContext(ctx context.Context, project models.CreateProject) (string, *models.Error)
-
 	// UpdateProject updates a project.
 	UpdateProject(project models.CreateProject) (string, *models.Error)
-
-	// UpdateProjectWithContext updates a project.
-	UpdateProjectWithContext(ctx context.Context, project models.CreateProject) (string, *models.Error)
 
 	// DeleteProject deletes a project.
 	DeleteProject(project models.Project) (*models.DeleteProjectResponse, *models.Error)
 
-	// DeleteProjectWithContext deletes a project.
-	DeleteProjectWithContext(ctx context.Context, project models.Project) (*models.DeleteProjectResponse, *models.Error)
-
 	// CreateService creates a new service.
 	CreateService(project string, service models.CreateService) (string, *models.Error)
-
-	// CreateServiceWithContext creates a new service.
-	CreateServiceWithContext(ctx context.Context, project string, service models.CreateService) (string, *models.Error)
 
 	// DeleteService deletes a service.
 	DeleteService(project string, service string) (*models.DeleteServiceResponse, *models.Error)
 
-	// DeleteServiceWithContext deletes a service.
-	DeleteServiceWithContext(ctx context.Context, project string, service string) (*models.DeleteServiceResponse, *models.Error)
-
 	// GetMetadata retrieves Keptn metadata information.
 	GetMetadata() (*models.Metadata, *models.Error)
+}
 
-	// GetMetadataWithContext retrieves Keptn metadata information.
-	GetMetadataWithContext(ctx context.Context) (*models.Metadata, *models.Error)
+// APIV2SendEventOptions are options for APIV2Interface.SendEvent().
+type APIV2SendEventOptions struct{}
+
+// APIV2TriggerEvaluationOptions are options for APIV2Interface.TriggerEvaluation().
+type APIV2TriggerEvaluationOptions struct{}
+
+// APIV2CreateProjectOptions are options for APIV2Interface.CreateProject().
+type APIV2CreateProjectOptions struct{}
+
+// APIV2UpdateProjectOptions are options for APIV2Interface.UpdateProject().
+type APIV2UpdateProjectOptions struct{}
+
+// APIV2DeleteProjectOptions are options for APIV2Interface.DeleteProject().
+type APIV2DeleteProjectOptions struct{}
+
+// APIV2CreateServiceOptions are options for APIV2Interface.CreateService().
+type APIV2CreateServiceOptions struct{}
+
+// APIV2DeleteServiceOptions are options for APIV2Interface.DeleteService().
+type APIV2DeleteServiceOptions struct{}
+
+// APIV2GetMetadataOptions are options for APIV2Interface.GetMetadata().
+type APIV2GetMetadataOptions struct{}
+
+type APIV2Interface interface {
+	// SendEvent sends an event to Keptn.
+	SendEvent(ctx context.Context, event models.KeptnContextExtendedCE, opts APIV2SendEventOptions) (*models.EventContext, *models.Error)
+
+	// TriggerEvaluation triggers a new evaluation.
+	TriggerEvaluation(ctx context.Context, project string, stage string, service string, evaluation models.Evaluation, opts APIV2TriggerEvaluationOptions) (*models.EventContext, *models.Error)
+
+	// CreateProject creates a new project.
+	CreateProject(ctx context.Context, project models.CreateProject, opts APIV2CreateProjectOptions) (string, *models.Error)
+
+	// UpdateProject updates a project.
+	UpdateProject(ctx context.Context, project models.CreateProject, opts APIV2UpdateProjectOptions) (string, *models.Error)
+
+	// DeleteProject deletes a project.
+	DeleteProject(ctx context.Context, project models.Project, opts APIV2DeleteProjectOptions) (*models.DeleteProjectResponse, *models.Error)
+
+	// CreateService creates a new service.
+	CreateService(ctx context.Context, project string, service models.CreateService, opts APIV2CreateServiceOptions) (string, *models.Error)
+
+	// DeleteService deletes a service.
+	DeleteService(ctx context.Context, project string, service string, opts APIV2DeleteServiceOptions) (*models.DeleteServiceResponse, *models.Error)
+
+	// GetMetadata retrieves Keptn metadata information.
+	GetMetadata(ctx context.Context, opts APIV2GetMetadataOptions) (*models.Metadata, *models.Error)
 }
 
 // APIHandler handles projects
 type APIHandler struct {
+	APIV2Handler
+}
+
+// APIV2Handler handles projects
+type APIV2Handler struct {
 	BaseURL    string
 	AuthToken  string
 	AuthHeader string
@@ -81,13 +112,19 @@ func NewAuthenticatedAPIHandler(baseURL string, authToken string, authHeader str
 }
 
 func createAuthenticatedAPIHandler(baseURL string, authToken string, authHeader string, httpClient *http.Client, scheme string) *APIHandler {
+	return &APIHandler{
+		APIV2Handler: *createAuthenticatedAPIV2Handler(baseURL, authToken, authHeader, httpClient, scheme),
+	}
+}
+
+func createAuthenticatedAPIV2Handler(baseURL string, authToken string, authHeader string, httpClient *http.Client, scheme string) *APIV2Handler {
 	baseURL = strings.TrimPrefix(baseURL, "http://")
 	baseURL = strings.TrimPrefix(baseURL, "https://")
 	if !strings.HasSuffix(baseURL, shipyardControllerBaseURL) {
 		baseURL += "/" + shipyardControllerBaseURL
 	}
 
-	return &APIHandler{
+	return &APIV2Handler{
 		BaseURL:    baseURL,
 		AuthHeader: authHeader,
 		AuthToken:  authToken,
@@ -96,29 +133,29 @@ func createAuthenticatedAPIHandler(baseURL string, authToken string, authHeader 
 	}
 }
 
-func (a *APIHandler) getBaseURL() string {
+func (a *APIV2Handler) getBaseURL() string {
 	return a.BaseURL
 }
 
-func (a *APIHandler) getAuthToken() string {
+func (a *APIV2Handler) getAuthToken() string {
 	return a.AuthToken
 }
 
-func (a *APIHandler) getAuthHeader() string {
+func (a *APIV2Handler) getAuthHeader() string {
 	return a.AuthHeader
 }
 
-func (a *APIHandler) getHTTPClient() *http.Client {
+func (a *APIV2Handler) getHTTPClient() *http.Client {
 	return a.HTTPClient
 }
 
 // SendEvent sends an event to Keptn.
 func (a *APIHandler) SendEvent(event models.KeptnContextExtendedCE) (*models.EventContext, *models.Error) {
-	return a.SendEventWithContext(context.TODO(), event)
+	return a.APIV2Handler.SendEvent(context.TODO(), event, APIV2SendEventOptions{})
 }
 
-// SendEventWithContext sends an event to Keptn.
-func (a *APIHandler) SendEventWithContext(ctx context.Context, event models.KeptnContextExtendedCE) (*models.EventContext, *models.Error) {
+// SendEvent sends an event to Keptn.
+func (a *APIV2Handler) SendEvent(ctx context.Context, event models.KeptnContextExtendedCE, opts APIV2SendEventOptions) (*models.EventContext, *models.Error) {
 	bodyStr, err := event.ToJSON()
 	if err != nil {
 		return nil, buildErrorResponse(err.Error())
@@ -135,11 +172,11 @@ func (a *APIHandler) SendEventWithContext(ctx context.Context, event models.Kept
 
 // TriggerEvaluation triggers a new evaluation.
 func (a *APIHandler) TriggerEvaluation(project, stage, service string, evaluation models.Evaluation) (*models.EventContext, *models.Error) {
-	return a.TriggerEvaluationWithContext(context.TODO(), project, stage, service, evaluation)
+	return a.APIV2Handler.TriggerEvaluation(context.TODO(), project, stage, service, evaluation, APIV2TriggerEvaluationOptions{})
 }
 
-// TriggerEvaluationWithContext triggers a new evaluation.
-func (a *APIHandler) TriggerEvaluationWithContext(ctx context.Context, project, stage, service string, evaluation models.Evaluation) (*models.EventContext, *models.Error) {
+// TriggerEvaluation triggers a new evaluation.
+func (a *APIV2Handler) TriggerEvaluation(ctx context.Context, project, stage, service string, evaluation models.Evaluation, opts APIV2TriggerEvaluationOptions) (*models.EventContext, *models.Error) {
 	bodyStr, err := evaluation.ToJSON()
 	if err != nil {
 		return nil, buildErrorResponse(err.Error())
@@ -149,11 +186,11 @@ func (a *APIHandler) TriggerEvaluationWithContext(ctx context.Context, project, 
 
 // CreateProject creates a new project.
 func (a *APIHandler) CreateProject(project models.CreateProject) (string, *models.Error) {
-	return a.CreateProjectWithContext(context.TODO(), project)
+	return a.APIV2Handler.CreateProject(context.TODO(), project, APIV2CreateProjectOptions{})
 }
 
-// CreateProjectWithContext creates a new project.
-func (a *APIHandler) CreateProjectWithContext(ctx context.Context, project models.CreateProject) (string, *models.Error) {
+// CreateProject creates a new project.
+func (a *APIV2Handler) CreateProject(ctx context.Context, project models.CreateProject, opts APIV2CreateProjectOptions) (string, *models.Error) {
 
 	bodyStr, err := project.ToJSON()
 	if err != nil {
@@ -164,11 +201,11 @@ func (a *APIHandler) CreateProjectWithContext(ctx context.Context, project model
 
 // UpdateProject updates a project.
 func (a *APIHandler) UpdateProject(project models.CreateProject) (string, *models.Error) {
-	return a.UpdateProjectWithContext(context.TODO(), project)
+	return a.APIV2Handler.UpdateProject(context.TODO(), project, APIV2UpdateProjectOptions{})
 }
 
-// UpdateProjectWithContext updates a project.
-func (a *APIHandler) UpdateProjectWithContext(ctx context.Context, project models.CreateProject) (string, *models.Error) {
+// UpdateProject updates a project.
+func (a *APIV2Handler) UpdateProject(ctx context.Context, project models.CreateProject, opts APIV2UpdateProjectOptions) (string, *models.Error) {
 	bodyStr, err := project.ToJSON()
 	if err != nil {
 		return "", buildErrorResponse(err.Error())
@@ -178,11 +215,11 @@ func (a *APIHandler) UpdateProjectWithContext(ctx context.Context, project model
 
 // DeleteProject deletes a project.
 func (a *APIHandler) DeleteProject(project models.Project) (*models.DeleteProjectResponse, *models.Error) {
-	return a.DeleteProjectWithContext(context.TODO(), project)
+	return a.APIV2Handler.DeleteProject(context.TODO(), project, APIV2DeleteProjectOptions{})
 }
 
-// DeleteProjectWithContext deletes a project.
-func (a *APIHandler) DeleteProjectWithContext(ctx context.Context, project models.Project) (*models.DeleteProjectResponse, *models.Error) {
+// DeleteProject deletes a project.
+func (a *APIV2Handler) DeleteProject(ctx context.Context, project models.Project, opts APIV2DeleteProjectOptions) (*models.DeleteProjectResponse, *models.Error) {
 	resp, err := delete(ctx, a.Scheme+"://"+a.getBaseURL()+v1ProjectPath+"/"+project.ProjectName, a)
 	if err != nil {
 		return nil, err
@@ -200,11 +237,11 @@ func (a *APIHandler) DeleteProjectWithContext(ctx context.Context, project model
 
 // CreateService creates a new service.
 func (a *APIHandler) CreateService(project string, service models.CreateService) (string, *models.Error) {
-	return a.CreateServiceWithContext(context.TODO(), project, service)
+	return a.APIV2Handler.CreateService(context.TODO(), project, service, APIV2CreateServiceOptions{})
 }
 
-// CreateServiceWithContext creates a new service.
-func (a *APIHandler) CreateServiceWithContext(ctx context.Context, project string, service models.CreateService) (string, *models.Error) {
+// CreateService creates a new service.
+func (a *APIV2Handler) CreateService(ctx context.Context, project string, service models.CreateService, opts APIV2CreateServiceOptions) (string, *models.Error) {
 	bodyStr, err := service.ToJSON()
 	if err != nil {
 		return "", buildErrorResponse(err.Error())
@@ -214,11 +251,11 @@ func (a *APIHandler) CreateServiceWithContext(ctx context.Context, project strin
 
 // DeleteService deletes a service.
 func (a *APIHandler) DeleteService(project, service string) (*models.DeleteServiceResponse, *models.Error) {
-	return a.DeleteServiceWithContext(context.TODO(), project, service)
+	return a.APIV2Handler.DeleteService(context.TODO(), project, service, APIV2DeleteServiceOptions{})
 }
 
-// DeleteServiceWithContext deletes a service.
-func (a *APIHandler) DeleteServiceWithContext(ctx context.Context, project, service string) (*models.DeleteServiceResponse, *models.Error) {
+// DeleteService deletes a service.
+func (a *APIV2Handler) DeleteService(ctx context.Context, project, service string, opts APIV2DeleteServiceOptions) (*models.DeleteServiceResponse, *models.Error) {
 	resp, err := delete(ctx, a.Scheme+"://"+a.getBaseURL()+v1ProjectPath+"/"+project+pathToService+"/"+service, a)
 	if err != nil {
 		return nil, err
@@ -236,11 +273,11 @@ func (a *APIHandler) DeleteServiceWithContext(ctx context.Context, project, serv
 
 // GetMetadata retrieves Keptn metadata information.
 func (a *APIHandler) GetMetadata() (*models.Metadata, *models.Error) {
-	return a.GetMetadataWithContext(context.TODO())
+	return a.APIV2Handler.GetMetadata(context.TODO(), APIV2GetMetadataOptions{})
 }
 
-// GetMetadataWithContext retrieves Keptn metadata information.
-func (a *APIHandler) GetMetadataWithContext(ctx context.Context) (*models.Metadata, *models.Error) {
+// GetMetadata retrieves Keptn metadata information.
+func (a *APIV2Handler) GetMetadata(ctx context.Context, opts APIV2GetMetadataOptions) (*models.Metadata, *models.Error) {
 	baseURL := a.getBaseURL()
 	if strings.HasSuffix(baseURL, "/"+shipyardControllerBaseURL) {
 		baseURL = strings.TrimSuffix(a.getBaseURL(), "/"+shipyardControllerBaseURL)
