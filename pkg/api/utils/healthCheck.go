c5e1b7519becaa8cd0f3501a4909df74f061843f
Florian Bacher <florian.bacher@dynatrace.com>
2022-05-12 10:24:58 +0200

    feat: Provide option to specify readiness condition (#464)
    * feat: Allow to specify readiness condition in health handler
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * added unit tests
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * add health check path option
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * adapted unit test
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * added comments to new options
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * added comment
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- pkg/api/utils/healthCheck.go
+++ pkg/api/utils/healthCheck.go
@@ -7,6 +7,10 @@ import (
 	"net/http"
 )
 
+const defaultHealthEndpointPath = "/health"
+
+type ReadinessConditionFunc func() bool
+
 type StatusBody struct {
 	Status string `json:"status"`
 }
@@ -16,7 +20,48 @@ func (s *StatusBody) ToJSON() ([]byte, error) {
 	return json.Marshal(s)
 }
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
+type HealthHandlerOption func(h *healthHandler)
+
+// WithReadinessConditionFunc allows to specify a function that should determine if the endpoint should return an HTTP 200 (OK), or
+// a 412 (Precondition failed) response
+func WithReadinessConditionFunc(rc ReadinessConditionFunc) HealthHandlerOption {
+	return func(h *healthHandler) {
+		h.readinessConditionFunc = rc
+	}
+}
+
+// WithPath allows to specify the path under which the endpoint should be reachable
+func WithPath(path string) HealthHandlerOption {
+	return func(h *healthHandler) {
+		h.path = path
+	}
+}
+
+type healthHandler struct {
+	readinessConditionFunc ReadinessConditionFunc
+	path                   string
+}
+
+func newHealthHandler(opts ...HealthHandlerOption) *healthHandler {
+	h := &healthHandler{
+		path: defaultHealthEndpointPath,
+	}
+	for _, o := range opts {
+		o(h)
+	}
+	return h
+}
+
+func (h *healthHandler) healthCheck(w http.ResponseWriter, r *http.Request) {
+	ready := true
+	if h.readinessConditionFunc != nil {
+		ready = h.readinessConditionFunc()
+	}
+
+	if !ready {
+		w.WriteHeader(http.StatusPreconditionFailed)
+		return
+	}
 	status := StatusBody{Status: "OK"}
 
 	body, err := status.ToJSON()
@@ -32,9 +77,11 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func RunHealthEndpoint(port string) {
-
-	http.HandleFunc("/health", healthHandler)
+// RunHealthEndpoint starts an http server on the specified port and provides a simple HTTP Get endpoint that can be used for health checks
+// per default, the endpoint will be reachable under the path '/health'
+func RunHealthEndpoint(port string, opts ...HealthHandlerOption) {
+	h := newHealthHandler(opts...)
+	http.HandleFunc(h.path, h.healthCheck)
 	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 	if err != nil {
 		log.Println(err)
