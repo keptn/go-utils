be85c90a72dd2957770bb461880bee44afb4ee73
Arthur Pitman <arthur.pitman@dynatrace.com>
2022-07-01 12:56:05 +0200

    Deduplicate constant
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- pkg/api/utils/errors.go
+++ pkg/api/utils/errors.go
@@ -1,4 +1,6 @@
 package api
 
+import v2 "github.com/keptn/go-utils/pkg/api/utils/v2"
+
 // ErrWithStatusCode message
-const ErrWithStatusCode = "error with status code %d"
+const ErrWithStatusCode = v2.ErrWithStatusCode
