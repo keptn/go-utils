6b8d8f22108199a622236195f76816924354fc84
RealAnna <89971034+RealAnna@users.noreply.github.com>
2023-02-23 11:49:41 +0100

    feat: add possibility to read mongo secret from file (#566)
    * feat: add possibility to read mongo secret from file
    
    Signed-off-by: realanna <anna.reale@dynatrace.com>
    
    * feat: add possibility to read mongo secret from file
    
    Signed-off-by: realanna <anna.reale@dynatrace.com>
    
    * Update pkg/common/mongoutils/mongo.go
    
    Co-authored-by: Giovanni Liva <giovanni.liva@dynatrace.com>
    Signed-off-by: realanna <anna.reale@dynatrace.com>
    
    * Update pkg/common/mongoutils/mongo.go
    
    Co-authored-by: Giovanni Liva <giovanni.liva@dynatrace.com>
    Signed-off-by: realanna <anna.reale@dynatrace.com>
    
    * review
    
    Signed-off-by: realanna <anna.reale@dynatrace.com>
    
    ---------
    
    Signed-off-by: realanna <anna.reale@dynatrace.com>
    Co-authored-by: Giovanni Liva <giovanni.liva@dynatrace.com>

--- pkg/common/mongoutils/mongo.go
+++ pkg/common/mongoutils/mongo.go
@@ -3,9 +3,16 @@ package mongoutils
 import (
 	"errors"
 	"fmt"
+
 	"github.com/keptn/go-utils/pkg/common/strutils"
+	logger "github.com/sirupsen/logrus"
+
 	"os"
 )
+
+const mongoUser = "/mongodb-user"
+const mongoPw = "/mongodb-passwords"
+const mongoExtCon = "/external_connection_string"
 
 // GetMongoConnectionStringFromEnv returns a mongodb connection string and the database name by considering the following environment variables:
 // MONGODB_DATABASE - The name of the database within the mongodb service (e.g. keptn)
@@ -20,15 +27,32 @@ func GetMongoConnectionStringFromEnv() (string, string, error) {
 	if mongoDBName == "" {
 		return "", "", errors.New("env var 'MONGODB_DATABASE' env var must be set")
 	}
-	if externalConnectionString := os.Getenv("MONGODB_EXTERNAL_CONNECTION_STRING"); externalConnectionString != "" {
+	configdir := os.Getenv("MONGO_CONFIG_DIR")
+
+	if externalConnectionString := getFromFileOrEnv("MONGODB_EXTERNAL_CONNECTION_STRING", configdir+mongoExtCon); externalConnectionString != "" {
 		return externalConnectionString, mongoDBName, nil
 	}
 	mongoDBHost := os.Getenv("MONGODB_HOST")
-	mongoDBUser := os.Getenv("MONGODB_USER")
-	mongoDBPassword := os.Getenv("MONGODB_PASSWORD")
+	mongoDBUser := getFromFileOrEnv("MONGODB_USER", configdir+mongoUser)
+	mongoDBPassword := getFromFileOrEnv("MONGODB_PASSWORD", configdir+mongoPw)
 
 	if !strutils.AllSet(mongoDBHost, mongoDBUser, mongoDBPassword) {
 		return "", "", errors.New("could not construct mongodb connection string: env vars 'MONGODB_HOST', 'MONGODB_USER' and 'MONGODB_PASSWORD' have to be set")
 	}
 	return fmt.Sprintf("mongodb://%s:%s@%s/%s", mongoDBUser, mongoDBPassword, mongoDBHost, mongoDBName), mongoDBName, nil
+}
+
+func readMountedSecret(file string) string {
+	body, err := os.ReadFile(file)
+	if err != nil {
+		logger.Fatalf("unable to read mounted secret: %v", err)
+	}
+	return string(body)
+}
+
+func getFromFileOrEnv(env string, path string) string {
+	if _, err := os.Stat(path); err == nil {
+		return readMountedSecret(path)
+	}
+	return os.Getenv(env)
 }
